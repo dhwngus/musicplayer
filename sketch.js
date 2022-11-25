ae8132b332441252d082c685924563c088c0e400
dhwngus <90364592+dhwngus@users.noreply.github.com>
2022-11-25 14:17:58 +0900

    오류sketch
    고침

--- sketch.js
+++ sketch.js
@@ -1,12 +1,31 @@
 let musicFile;
+let button;
 function setup()
 {
+    createCanvas(640,400);
     soundFormats('mp3','ogg');
     musicFile = loadSound('who',loadMusic);
     // musicFile.play();
-}
+    button = createButton("play");
+    button.mousePressed(togglePlaying);
 
+}
+function togglePlaying(){
+    if(!musicFile.isPlaying()){
+        musicFile.play();
+        button.html('pause');
+    }else{
+        musicFile.stop();
+        button.html('play');
+    }
+}
 function loadMusic()
 {
-    musicFile.play(); //문빈산하
+    console/log("it's working");
+    // musicFile.play(); //문빈산하
+    
+}
+function draw(){
+    fill(2555,0,0);
+ellipse(50,50,100,100);
 }
